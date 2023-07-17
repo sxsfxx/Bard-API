81cce2c5fffb4bd4511e684aaa24e63f789855f5
luj <sxsfxx@gmail.com>
2023-07-17 15:26:48 +0800

    1) update the correct string.

--- examples/webard.py
+++ examples/webard.py
@@ -1,5 +1,5 @@
 from bardapi import Bard
 
-token = 'sidts-CjEBPu3jId5TpMgIrxLRekMb6nVWUuSG4qjU9PdNEi04R7YzdOPaSBpe1bQmu2d78zvTEAA'
+token = 'YwgC030KE0JFi41YY9iqOJ44y6uuKjZSQz6g8FEFMKmIY8SicAYQBLAlMv-A4yD6RjE23A.'
 bard = Bard(token=token)
 bard.get_answer("请告诉我世界上最高的山峰是哪一个？")['content']
