6534313c423b429985e99369d7789d621a23ef5b
luj <sxsfxx@gmail.com>
2023-07-17 09:11:55 +0100

    1) rename the project name/directory.

--- examples/webard.py
+++
@@ -1,5 +0,0 @@
-from bardapi import Bard
-
-token = 'YwgC030KE0JFi41YY9iqOJ44y6uuKjZSQz6g8FEFMKmIY8SicAYQBLAlMv-A4yD6RjE23A.'
-bard = Bard(token=token)
-bard.get_answer("请告诉我世界上最高的山峰是哪一个？")['content']
