84395068949cc82b49a78daaa48e6f8334423d15
luj <sxsfxx@gmail.com>
2023-07-21 10:17:51 +0800

    1) index page.

---
+++ webard/app.py
@@ -0,0 +1,25 @@
+from bardapi import Bard
+from flask import Flask
+from flask import request
+from flask import render_template
+
+token = 'YwgC030KE0JFi41YY9iqOJ44y6uuKjZSQz6g8FEFMKmIY8SicAYQBLAlMv-A4yD6RjE23A.'
+bard = Bard(token=token)
+#s = bard.get_answer("请告诉我世界上最高的山峰是哪一个？")['content']
+#print(s)
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    kvs = {}
+    if request.method == "POST":
+        q = request.form.get("q")
+        if q:
+            try:
+                a = bard.get_answer(q)["content"]
+                if a:
+                    kvs.update({q:a})
+            except:
+                kvs.update({q:"error"})
+    return render_template("index.html", kvs=kvs)
